157ec5346e453a0ad562f4c5c9072de31a6fe5ac
reemsh2000 <sh951652@gmail.com>
2022-11-24 10:30:19 +0200

    remove useless components & add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
 import { GMapModule } from 'primeng/gmap';
 import { AppComponent } from './app.component';
-import { SocialNavComponent } from './social-nav/social-nav.component';
 import { NavComponent } from './nav/nav.component';
-import { SearchNavComponent } from './search-nav/search-nav.component';
 import { MapComponent } from './map/map.component';
 import { ToastModule } from 'primeng/toast';
 import { DialogModule } from 'primeng/dialog';
@@ -17,9 +15,7 @@ import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    SocialNavComponent,
     NavComponent,
-    SearchNavComponent,
     MapComponent,
   ],
   imports: [

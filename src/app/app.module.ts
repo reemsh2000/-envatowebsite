e66d576e1d130ef8db77bdb401c0ae89b1f3d85c
reemsh2000 <sh951652@gmail.com>
2022-11-27 13:45:41 +0200

    styles(css files) control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CarouselModule } from 'primeng/carousel';
 import { ButtonModule } from 'primeng/button';
@@ -8,6 +10,12 @@ import { ToastModule } from 'primeng/toast';
 import { DialogModule } from 'primeng/dialog';
 import { CheckboxModule } from 'primeng/checkbox';
 import { CardModule } from 'primeng/card';
+import { DataViewModule } from 'primeng/dataview';
+import { PanelModule } from 'primeng/panel';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { RatingModule } from 'primeng/rating';
+import { RippleModule } from 'primeng/ripple';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './common/nav/nav.component';
@@ -16,7 +24,8 @@ import { FormsModule } from '@angular/forms';
 import { CarouselComponent } from './carousel/carousel.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { CardComponent } from './common/card/card.component';
-
+import { DataviewComponent } from './dataview/dataview.component';
+import { ProductService } from './dataview/product.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,8 +34,10 @@ import { CardComponent } from './common/card/card.component';
     CarouselComponent,
     WelcomeComponent,
     CardComponent,
+    DataviewComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     ButtonModule,
     GMapModule,
@@ -37,8 +48,14 @@ import { CardComponent } from './common/card/card.component';
     BrowserAnimationsModule,
     CarouselModule,
     CardModule,
+    DataViewModule,
+    InputTextModule,
+    PanelModule,
+    RatingModule,
+    DropdownModule,
+    RippleModule,
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

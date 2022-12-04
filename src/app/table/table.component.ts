35d88cbeb958cd16d2672e3693281d2f8b7e0f78
reemsh2000 <sh951652@gmail.com>
2022-12-04 11:17:14 +0200

    Add table component

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer, Representative } from './customer';
+import { CustomerService } from './customer.service';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss'],
+})
+export class TableComponent implements OnInit {
+  customers: Customer[] = [];
+
+  representatives: Representative[] = [];
+
+  statuses: any[] = [];
+
+  loading: boolean = true;
+
+  activityValues: number[] = [0, 100];
+
+  constructor(private customerService: CustomerService) {}
+
+  ngOnInit() {
+    this.customerService.getCustomersLarge().then((customers) => {
+      this.customers = customers;
+      this.loading = false;
+
+      this.customers.forEach(
+        (customer: any) => (customer.date = new Date(customer.date))
+      );
+    });
+
+    this.representatives = [
+      { name: 'Amy Elsner', image: 'amyelsner.png' },
+      { name: 'Anna Fali', image: 'annafali.png' },
+      { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
+      { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
+      { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
+      { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
+      { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
+      { name: 'Onyama Limba', image: 'onyamalimba.png' },
+      { name: 'Stephen Shaw', image: 'stephenshaw.png' },
+      { name: 'XuXue Feng', image: 'xuxuefeng.png' },
+    ];
+
+    this.statuses = [
+      { label: 'Unqualified', value: 'unqualified' },
+      { label: 'Qualified', value: 'qualified' },
+      { label: 'New', value: 'new' },
+      { label: 'Negotiation', value: 'negotiation' },
+      { label: 'Renewal', value: 'renewal' },
+      { label: 'Proposal', value: 'proposal' },
+    ];
+  }
+}
